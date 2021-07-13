d6e4a3dbb9a91aeb0a57f2ecaf2094bafe55e391
maximum <454>
2021-07-13 23:22:03 +0600

    fix

--- src/js/school.js
+++ src/js/school.js
@@ -38,4 +38,45 @@ $(function () {
     }
     const productsCarousel = $(".products-carousel").slick(slickSetting);
 
+    const promoSetting = {
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+        slidesPerRow: 1,
+        rows: 2,
+        responsive: [
+            {
+                breakpoint: 1023,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    rows: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    slidesPerRow: 1,
+                    rows: 2,
+                }
+            }
+        ]
+    }
+    const productsPromo = $(".products-promo-slider").slick(promoSetting);
+
+
+
 })
